bb97a57fe0d7f0ad226aa974fc11205c92ed2b94
murloctidecaller <102994544+murloctidecaller@users.noreply.github.com>
2022-05-29 21:04:09 +0300

    refactor: рефакторинг заглавной буквы класса

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         int ticket = 200;
@@ -7,4 +7,4 @@ public class main {
         int bonus = ticket / oneMileRuble;
         System.out.println("Итоговый бонус: " + bonus);
     }
-    }
+}
